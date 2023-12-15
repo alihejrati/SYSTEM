d0d10aba2887d03cb4d2908800724388afaf3998
alihejrati <alihejrati1996@gmail.com>
2023-12-15 03:33:47 +0330

    .

--- DATA/OPENCV/app/fundus/od_fov_rot.py
+++ DATA/OPENCV/app/fundus/od_fov_rot.py
@@ -127,8 +127,8 @@ class FundusROT(DIP):
 if __name__ == '__main__':
     from . import ROOT_DIR
     FundusROT(
-        # DIP_SPATH='/home/alihejrati/Documents/Dataset/fundus - RetinaLessions/retinal-lesions-v20191227/images_896x896/*.jpg',
-        DIP_SPATH='/content/retinal-lesions-v20191227/images_896x896/*.jpg',
+        DIP_SPATH='/home/alihejrati/Documents/Dataset/fundus - RetinaLessions/retinal-lesions-v20191227/images_896x896/*.jpg',
+        # DIP_SPATH='/content/retinal-lesions-v20191227/images_896x896/*.jpg',
         DIP_SPATH_HEAD=30,
         DIP_DF_SPATH=f'{ROOT_DIR}/export/RetinaLessions.csv',
         DIP_DPATH=f'{ROOT_DIR}/export/RetinaLessions',
